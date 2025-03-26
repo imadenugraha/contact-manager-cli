48362472b6372232d65f9372ae2ef1158d3d1c7f
imadenugraha <imadenugraha@gmail.com>
2025-03-26 23:50:44 +0700

    refactor: using display for enum type and optimize code

--- src/contacts.rs
+++ src/contacts.rs
@@ -1,6 +1,5 @@
-use std::io;
+use std::{fmt, io};
 
-#[derive(Debug)]
 pub enum ContactType {
     Work,
     Home,
@@ -13,16 +12,27 @@ pub struct Contact {
     contact_type: ContactType
 }
 
+impl fmt::Display for ContactType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let type_str = match self {
+            ContactType::Home => "Home",
+            ContactType::Work => "Work",
+            ContactType::Other => "Other",
+        };
+        write!(f, "{}", type_str)
+    }
+}
+
 impl Contact {
     pub fn add(contacts: &mut Vec<Self>, name: String, phone: String, contact_type: ContactType) {
         contacts.push(Self { name, phone, contact_type })
     }
 
     pub fn search(contacts: &[Self], name: String) {
-        let _ = match contacts.iter().find(|contact| contact.name.to_lowercase() == name.to_lowercase()) {
+        match contacts.iter().find(|contact| contact.name.to_lowercase() == name.to_lowercase()) {
             Some(contact) => {
                 println!("Contact ditemukan!\n");
-                println!("{} - {} [{:?}]", contact.name, contact.phone, contact.contact_type);
+                println!("{} - {} [{}]", contact.name, contact.phone, contact.contact_type);
             },
             None => {
                 println!("Contact tidak ditemukan!\n");
@@ -32,7 +42,7 @@ impl Contact {
     }
 
     pub fn delete(contacts: &mut Vec<Self>, name: String) {
-        let _ = match contacts.iter_mut().position(|contact| contact.name.to_lowercase() == name.to_lowercase()) {
+        match contacts.iter_mut().position(|contact| contact.name.to_lowercase() == name.to_lowercase()) {
             Some(contact) => {
                 contacts.remove(contact);
                 println!("Contact berhasil dihapus!\n");
@@ -49,18 +59,21 @@ pub fn add_contact(contacts: &mut Vec<Contact>) {
     println!("Contact Name: ");
     let mut name: String = String::new();
     io::stdin().read_line(&mut name).expect("Cannot read input!");
+    let name: String = name.trim().to_string();
 
     println!("Phone Number: ");
     let mut phone: String = String::new();
     io::stdin().read_line(&mut phone).expect("Cannot read input!");
+    let phone: String = phone.trim().to_string();
 
     println!("Contact Type => (1) Work (2) Home (3) Other: ");
     let mut contact_type: String = String::new();
     io::stdin().read_line(&mut contact_type).expect("Cannot read input!");
+    let contact_type: String = contact_type.trim().to_string();
 
     match contact_type.trim() {
-        "1" => Contact::add(contacts, name, phone, ContactType::Home),
-        "2" => Contact::add(contacts, name, phone, ContactType::Work),
+        "1" => Contact::add(contacts, name, phone, ContactType::Work),
+        "2" => Contact::add(contacts, name, phone, ContactType::Home),
         "3" => Contact::add(contacts, name, phone, ContactType::Other),
         _ => {
             println!("Invalid contact type!");
@@ -72,9 +85,14 @@ pub fn add_contact(contacts: &mut Vec<Contact>) {
 }
 
 pub fn list_contact(contacts: &[Contact]) {
+    if contacts.is_empty() {
+        println!("Contact empty!");
+        return;
+    }
+
     println!("Daftar Contact: ");
     for (num, contact) in contacts.iter().enumerate() {
-        println!("{}. {} - {} [{:?}]", num + 1, contact.name, contact.phone, contact.contact_type);
+        println!("{}. {} - {} [{}]", num + 1, contact.name, contact.phone, contact.contact_type);
     };
 }
 
